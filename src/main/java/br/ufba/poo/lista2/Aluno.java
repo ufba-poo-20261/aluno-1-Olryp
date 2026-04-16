b2600107f565b624b37d75ba9b18cf74f54260cc
Olryp <olrypaul@ufba.br>
2026-04-16 12:46:34 -0300

    Update Aluno.java

--- src/main/java/br/ufba/poo/lista2/Aluno.java
+++ src/main/java/br/ufba/poo/lista2/Aluno.java
@@ -1,22 +1,36 @@
 package br.ufba.poo.lista2;
 
-/**
- * Crie a classe `Aluno`, que possui os atributos `matricula` e `nome` (ambos do
- * tipo `String`), além dos métodos e construtores relevantes de acordo com a
- * seguinte especificação:
- *
- * - O nome do aluno pode ser alterado, mas nunca o seu número de matrícula
- * - O número de matrícula de um aluno deve poder ser acessado por outras partes
- * do programa
- * - Duas instâncias de `Aluno` são consideradas iguais se possuem o mesmo número
- * de matrícula
- *
- * Assuma, para este exercício, que a classe `Aluno` sempre será instanciada com um
- * número de matrícula e um nome válidos, passados nessa ordem.
- */
 public class Aluno {
 
+    private String matricula;
+    private String nome;
+
     public Aluno(String matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    // matrícula só pode ser lida (não pode mudar)
+    public String getMatricula() {
+        return matricula;
+    }
+
+    // nome pode ser lido
+    public String getNome() {
+        return nome;
+    }
+
+    // nome pode ser alterado
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Aluno outro = (Aluno) obj;
+        return this.matricula.equals(outro.matricula);
     }
 }
-
